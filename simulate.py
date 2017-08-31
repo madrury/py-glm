58ab885d1e2c79c5b3ad312c62a63e7e75a450eb
madrury <matthew.drury.83@gmail.com>
2017-08-30 20:48:41 -0700

    Add non_parametric_bootstrap

--- simulate.py
+++ simulate.py
@@ -13,7 +13,7 @@ class Simulation:
             simulations[i, :] = self.glm.family.sample(preds, self.glm.dispersion_)
         return simulations
 
-    def parameteric_bootstrap(self, X, n_sim=100, offset=None):
+    def parametric_bootstrap(self, X, n_sim=100, offset=None):
         simulations = self.sample(X, n_sim, offset=offset)
         models = []
         for i in range(n_sim):
@@ -21,3 +21,18 @@ class Simulation:
             model.fit(X, simulations[i, :], offset=offset)
             models.append(model)
         return models
+
+    def non_parametric_bootstrap(self, X, y, n_sim=100, offset=None):
+        models = []
+        for _ in range(n_sim):
+            idxs = np.random.choice(X.shape[0], X.shape[0])
+            X_boot, y_boot = X[idxs], y[idxs]
+            if offset:
+                offset_boot = offset_boot[idxs]
+            else:
+                offset_boot = None
+            model = self.glm.clone()
+            model.fit(X_boot, y_boot, offset=offset_boot)
+            models.append(model)
+        return models
+        
