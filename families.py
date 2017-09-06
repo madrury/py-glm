Fix deviance calculation for Gaussian.
The factor of 2 was incorrect.

families.py
@@ -73,7 +73,7 @@ class Gaussian(ExponentialFamily, ExponentialFamilyMixin):
         return np.ones(shape=mu.shape)
 
     def deviance(self, y, mu):
-        return 2 * np.sum((y - mu)**2)
+        return np.sum((y - mu)**2)
 
     def sample(self, mus, dispersion):
         return np.random.normal(mus, np.sqrt(dispersion))

glm.py
@@ -223,7 +223,7 @@ class GLM:
             raise ValueError("Dispersion parameter can only be estimated for a"
                              "fit model.")
         if self.family.has_dispersion:
-            return 0.5 * self.deviance_ / (self.n - self.p)
+            return self.deviance_ / (self.n - self.p)
         else:
             return np.ones(shape=self.deviance_.shape)
 