8bebcc2c0be291bb946a6e880f7d5098c545379e
madrury <matthew.drury.83@gmail.com>
2017-09-17 13:40:42 -0700

    Add include_intercept arguments.
    Allow for intecepts to be included in X when
    generating data for tests.

--- tests/generate_data.py
+++ tests/generate_data.py
@@ -4,8 +4,10 @@ from scipy.linalg import sqrtm
 def make_linear_regression(n_samples=10000,
                            n_uncorr_features=10, n_corr_features=10,
                            n_drop_features=4,
+                           include_intercept=True,
                            resid_sd=0.25):
-    X = make_correlated_data(n_samples, n_uncorr_features, n_corr_features)
+    X = make_correlated_data(
+        n_samples, n_uncorr_features, n_corr_features, include_intercept)
     parameters = make_regression_coeffs(X, n_drop_features=n_drop_features)
     y = make_linear_regression_y(X, parameters, resid_sd)
     return (X, y, parameters)
@@ -13,8 +15,10 @@ def make_linear_regression(n_samples=10000,
 def make_logistic_regression(n_samples=10000,
                              n_uncorr_features=10, n_corr_features=10,
                              n_drop_features=4,
+                             include_intercept=True,
                              resid_sd=0.25):
-    X = make_correlated_data(n_samples, n_uncorr_features, n_corr_features)
+    X = make_correlated_data(
+        n_samples, n_uncorr_features, n_corr_features, include_intercept)
     parameters = make_regression_coeffs(X, n_drop_features=n_drop_features)
     y = make_logistic_regression_y(X, parameters)
     return (X, y, parameters)
@@ -24,11 +28,16 @@ def make_uncorrelated_data(n_samples=10000, n_features=25):
     return X
 
 def make_correlated_data(n_samples=10000,
-                         n_uncorr_features=10, n_corr_features=15):
+                         n_uncorr_features=10, n_corr_features=15,
+                         include_intercept=True):
     X_uncorr = make_uncorrelated_data(n_samples, n_uncorr_features)
     cov_matrix = make_covariance_matrix(n_uncorr_features)
     X_corr = np.dot(X_uncorr, cov_matrix)
-    return np.column_stack((X_uncorr, X_corr))
+    X = np.column_stack((X_uncorr, X_corr))
+    if include_intercept:
+        intecept = np.ones(n_samples).reshape(-1, 1)
+        return np.column_stack((intecept, X))
+    return X
 
 def make_covariance_matrix(n_features=15):
     A = np.random.normal(size=(n_features, n_features))
@@ -37,20 +46,19 @@ def make_covariance_matrix(n_features=15):
 
 def make_regression_coeffs(X, n_drop_features=None):
     n_features = X.shape[1]
-    intercept =  np.random.uniform(-2, 2, size=1)
     parameters = np.random.uniform(-5, 5, size=n_features)
     if n_drop_features is not None:
         drop_idxs = np.random.choice(
             list(range(len(parameters))), size=n_drop_features, replace=False)
         parameters[drop_idxs] = 0.0
-    return intercept, parameters
+    return parameters
 
 def make_linear_regression_y(X, parameters, resid_sd=0.25):
-    y_systematic = parameters[0] + np.dot(X, parameters[1])
+    y_systematic = np.dot(X, parameters)
     y = y_systematic + np.random.normal(scale=resid_sd, size=X.shape[0])
     return y
 
 def make_logistic_regression_y(X, parameters):
-    y_systematic = parameters[0] + np.dot(X, parameters[1])
+    y_systematic = np.dot(X, parameters)
     p = 1 / (1 + np.exp(- y_systematic))
     return np.random.binomial(1, p)
