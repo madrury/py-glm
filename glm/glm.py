90ebfe8b8193ad43d2c5ff320932297eead8d63c
Matthew Drury <matthew.drury.83@gmail.com>
2020-04-22 14:43:42 -0700

    Fix spacing on GLM summary table.

--- glm/glm.py
+++ glm/glm.py
@@ -329,13 +329,13 @@ class GLM:
         variable_names = self.X_names
         parameter_estimates = self.coef_
         standard_errors = self.coef_standard_error_
-        header_string = "{:<10} {:>20} {:>15}".format(
+        longest_var_name_length = max(len(name) + 2 for name in variable_names)
+        header_string = "{:<" + str(longest_var_name_length + 2) + "{:>10} {:>15}".format(
             "Name", "Parameter Estimate", "Standard Error")
         print(f"{self.family.__class__.__name__} GLM Model Summary.")
         print('=' * len(header_string))
         print(header_string)
         print('-' * len(header_string))
-        longest_var_name_length = max(len(name) + 2 for name in variable_names)
         format_string = "{:<" + str(longest_var_name_length) + "} {:>10.2f} {:>15.2f}"
         for name, est, se in zip(variable_names, parameter_estimates, standard_errors):
             print(format_string.format(name, est, se))
